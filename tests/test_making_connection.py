6215ad1542c85187ce99be9f52947ee615ce35fd
birdi <egor.birdi7@gmail.com>
2019-08-15 16:19:50 +0300

    Upgrade basic tests of initing mongo client

--- tests/test_making_connection.py
+++ tests/test_making_connection.py
@@ -15,8 +15,7 @@ def mock_mongo():
 
 servers = ["server.example.com", "server2.example.com"]
 ports = [27017, 27024]
-invalid_ports = map(str, ports)
-
+sockets = zip(servers, ports)
 
 @pytest.fixture
 def test_data():
@@ -50,11 +49,12 @@ class TestBasicConnection:
     @my_params
     def test_establishing_invalid_connection(self, server_name, port, test_data):
         with pytest.raises(pymongo.errors.ServerSelectionTimeoutError):
-            client = get_client(server_name, port, serverSelectionTimeoutMS=1)
+            client: MongoClient = get_client(server_name, port, serverSelectionTimeoutMS=1)
+            assert client.server_selection_timeout == 1
             client.test_db.collect.insert_one(test_data)
 
     @pytest.mark.parametrize("server_name", servers)
-    @pytest.mark.parametrize("port", invalid_ports)
+    @pytest.mark.parametrize("port", map(str, ports))
     def test_invalid_ports(self, server_name, port):
         with pytest.raises(TypeError):
             client = get_client(server_name, port, serverSelectionTimeoutMS=1)
@@ -64,8 +64,10 @@ class TestBasicConnection:
         client1 = get_client(server_name, port)
         assert isinstance(client1, MongoClient)
 
+        assert client1.HOST == server_name
+        assert client1.PORT == port
         client2 = get_client(server_name, port)
-        assert client1 is client2
+        assert client1 is client2, "Two different client were given for the same (host, port) combination"
 
         new_server_name, new_port = random.choice(zip(servers, ports))
         # if I put new (host, port) it should be an error
